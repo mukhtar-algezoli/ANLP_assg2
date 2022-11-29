df352258958a332c6a341417f83b32f39d07725b
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2022-11-29 02:44:29 +0000

    adding viterbi function

--- label_slots.py
+++ label_slots.py
@@ -16,6 +16,48 @@ VALIDATION_FILE = "validation_data.json"
 spacy.tokens.token.Token.set_extension("bio_slot_label", default="O")
 
 
+def viterbi_algorithm(observations, states, start_p, trans_p, emit_p):
+    V = [{}]
+    print(observations)
+    for st in states:
+        #  print(observations[0])
+        #  print(emit_p[st])
+        #  print(emit_p[st][observations[0]])
+         V[0][st] = {"prob": start_p[st] * emit_p[st][observations[0]], "prev": None}
+   
+    for t in range(1, len(observations)):
+         V.append({})
+         for st in states:
+            max_tr_prob = V[t - 1][states[0]]["prob"] * trans_p[states[0]][st]
+            prev_st_selected = states[0]
+            for prev_st in states[1:]:
+                tr_prob = V[t - 1][prev_st]["prob"] * trans_p[prev_st][st]
+                if tr_prob > max_tr_prob:
+                    max_tr_prob = tr_prob
+                    prev_st_selected = prev_st
+ 
+            max_prob = max_tr_prob * emit_p[st][observations[t]]
+            V[t][st] = {"prob": max_prob, "prev": prev_st_selected}
+ 
+    opt = []
+    max_prob = 0.0
+    best_st = None
+ 
+    for st, data in V[-1].items():
+        if data["prob"] > max_prob:
+            max_prob = data["prob"]
+            best_st = st
+    opt.append(best_st)
+    previous = best_st
+ 
+ 
+    for t in range(len(V) - 2, -1, -1):
+        opt.insert(0, V[t + 1][previous]["prev"])
+        previous = V[t + 1][previous]["prev"]
+    
+    return opt
+
+
 def _predict_tag_mle(token, model_parameters):
     """
     Predict most-frequent-tag baseline, i.e. argmax_{<tag>} P(<tag> | <word>).
